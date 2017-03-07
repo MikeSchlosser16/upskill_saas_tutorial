044d7d9e1af6eca0b46b114a458714ec96675d5e
clear <schlo201@d.umn.edu>
2017-03-07 22:34:19 +0000

    Attempting to fix URL but need to deploy to heroku from main...

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://powerful-escarpment-24499.herokuapp.com/',
+  :domain         => 'powerful-escarpment-24499.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
